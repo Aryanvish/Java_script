b26b582d1daea6d5b98d5205cfb68d0426fb1175
Admin <aryanvishwakarma0622@gmail.com>
2025-03-11 13:55:10 +0530

    second question ch2

--- ch2ex.js
+++ ch2ex.js
@@ -1,3 +1,14 @@
-for(let m="#";m<"########";m=m+"#"){
-    console.log(m)
+for(n=1;n<101;n=n+1){
+    if(n%3==0 && n%5==0){
+        console.log("fizzbuzz")
+    }
+    elseif(n%5==0) {
+        console.log("buzz")
+    }
+    elseif(n%3==0) {
+        console.log("fizz")
+    }
+    else{
+         console.log(n)
+    }
 }
