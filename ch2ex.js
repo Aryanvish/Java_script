4edbc126d3d6ee70e27206e9a9d8ad50e9007b0a
Admin <aryanvishwakarma0622@gmail.com>
2025-03-11 14:26:07 +0530

    third question ch2

--- ch2ex.js
+++ ch2ex.js
@@ -1,14 +1,15 @@
-for(n=1;n<101;n=n+1){
-    if(n%3==0 && n%5==0){
-        console.log("fizzbuzz")
+let n='';
+for(let m=0;m<8;m=m+1){
+    let ver ='';
+    for(let l=0;l<8;l=l+1){
+        if((m+l)%2===0){
+            ver=ver+"#";
+
+        }else{
+            ver=ver+'';
+
+        }
     }
-    elseif(n%5==0) {
-        console.log("buzz")
+    n+=ver+'\n';
     }
-    elseif(n%3==0) {
-        console.log("fizz")
-    }
-    else{
-         console.log(n)
-    }
-}
+    console.log(n);
