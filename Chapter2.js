0e32b92ab63b422a4ce8b276c70bf37a0e7b0794
unknown <aryanvishwakarma0622@gmail.com>
2025-03-25 11:17:24 +0530

    Chapter 2 js

---
+++ Chapter2.js
@@ -0,0 +1,21 @@
+// Expressions ,conditionsand operators
+// Q1 use of logical operator to find age of person lies between 10 and 20
+// let age =prompt("whats your age?")
+// if(age>=10&&age<=20){
+//     console,log("true")
+// }
+// else{
+//     console,log("false")
+// }
+//switch case statements in js 
+switch(age=2){
+    case 1:
+        console.log("happy")
+    case 2:
+        console.log("sad")
+        break
+    deafault:
+    console.log("boring")    
+}
+
+
