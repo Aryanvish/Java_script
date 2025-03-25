72e3a2b2bf41c5fd0aeabdb0d1a6efa8c5cc01d1
unknown <aryanvishwakarma0622@gmail.com>
2025-03-25 06:00:25 +0530

    js chapter 1 questions

--- Chapter1.js
+++ Chapter1.js
@@ -1,6 +1,18 @@
-const item={
-    name:"Aryan",
-    phone_no:"8149109341",
-    marks:"8.64 sgpa"
+// q1 practice set 1 chapter 1
+let name="23.76"
+console.log(name+19)
+// q2 practice set 1 chapter 1
+console.log(typeof (name))
+// q3 practice set 1 chapter 1
+const a1={
+    name: "Aryan",
+    age:19,
+    
 }
-console.log(item)
+a=12//cannot change const object to hold a number value later 
+// q4 practice set 1 chapter 1 try to add new key to const object 
+a1['gender']="Male"
+a1['name']="sobo"
+console.log(a1)
+
+
